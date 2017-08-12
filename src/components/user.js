4bb798003b134820d685a3002a2bbb31a9d1ea66
Devin McGloin <devin@devinmcgloin.com>
2017-08-11 19:14:12 -0700

    Updated user page

--- src/components/user.js
+++ src/components/user.js
@@ -3,6 +3,7 @@ import FontAwesome from 'react-fontawesome'
 import PropTypes from 'prop-types'
 import {Collection} from "./collection";
 import ReactLoading from 'react-loading'
+import {Link} from 'react-router-dom'
 
 const User = ({user, images, isGrid, isLoading, isSummary}) => {
     if (isLoading){
@@ -10,14 +11,52 @@ const User = ({user, images, isGrid, isLoading, isSummary}) => {
     }
     else {
         return (
-            <div>
-                <h1>{user.name}</h1>
-                <h2>{user.bio}</h2>
-                <FontAwesome name="external-link">
-                    <a href={user.url}/>
-                </FontAwesome>
-                <Collection isGrid={isGrid} isLoading={isLoading} summary={isSummary} images={images}/>
-            </div>
+            <main className="cf pa3 pa4-m pa5-l mw9 center">
+                <div className="fr w-100 w-80-l">
+                    <p className="sans-serif f6">
+                        {user.bio}
+                    </p>
+                    <h1 className="sans-serif f2 f1-l lh-title mt0 mb4 mb5-ns">
+                        {user.name}
+                    </h1>
+                </div>
+                <div className="f6 lh-copy fl w-100 mb4">
+                    <div className="fl-ns w-100 w-20-l pr3-m pr5-l">
+                        <p>
+                            <Link className="sans-serif link dim gray" to={user.url}>
+                            <FontAwesome name="link"/> Personal Site
+                            </Link>
+                        </p>
+                    </div>
+                    <div className="fl-ns w-50-m w-20-l pr3-m pr5-l">
+                        <p>
+                            <Link className="sans-serif link dim gray" to={user.permalink + "/map"} >
+                                <FontAwesome name="map-o"/> Images Geo
+                            </Link>
+                        </p>
+                    </div>
+                    <div className="fl-ns w-50-m w-20-l pr3-m pr5-l">
+                        <p>
+                            <Link className="sans-serif link dim gray" to={user.permalink + "/favorites"} >
+                                <FontAwesome name="heart-o"/> Favorites
+                            </Link>
+                        </p>
+                    </div>
+                    <div className="fl-ns w-50-m w-20-l pr3-m pr5-l">
+                        <p className="sans-serif link dim gray">
+                            Member since {user.created_at}
+                        </p>
+                    </div>
+                    <div className="fl-ns w-50-m w-20-l pr3-m pr5-l">
+                        <p>
+                            <Link className="sans-serif link dim gray" to={user.permalink + "/stats"} >
+                                <FontAwesome name="line-chart"/> Stats
+                            </Link>
+                        </p>
+                    </div>
+                </div>
+                <Collection images={images} isGrid={isGrid} isLoading={isLoading} summary={isSummary}/>
+            </main>
         )
     }
 };
