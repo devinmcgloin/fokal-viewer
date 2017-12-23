89e445c53a0ca783d4425e277ddd4bd2eb2a79f6
Devin McGloin <devin@devinmcgloin.com>
2017-12-23 10:47:28 -0800

    Hide column controlls on mobile

--- src/components/collectionControls.js
+++ src/components/collectionControls.js
@@ -1,31 +1,57 @@
-import React from 'react'
-import FontAwesome from 'react-fontawesome'
-import PropTypes from 'prop-types'
-import {Link} from 'react-router-dom'
+import React from "react";
+import FontAwesome from "react-fontawesome";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-const Controls = ({options, selected, layout, handleLayoutChange, handleTypeChange, query}) => {
-    const control = options.map(o =>
-        <Link key={o.tag} to={query ? { pathname: o.link, search: query} : o.link}>
-        <span
-            className={"fl f5 link dim b no-underline black-50 dib pr2 pr3-ns pointer ttc " +
-            (o.tag === selected ? "black" : "")}
-            onClick={() => handleTypeChange(o.tag)}>{o.tag}</span>
-        </Link>);
+const Controls = ({
+    options,
+    selected,
+    layout,
+    handleLayoutChange,
+    handleTypeChange,
+    query
+}) => {
+    const control = options.map(o => (
+        <Link
+            key={o.tag}
+            to={query ? { pathname: o.link, search: query } : o.link}
+        >
+            <span
+                className={
+                    "fl f5 link dim b no-underline black-50 dib pr2 pr3-ns pointer ttc " +
+                    (o.tag === selected ? "black" : "")
+                }
+                onClick={() => handleTypeChange(o.tag)}
+            >
+                {o.tag}
+            </span>
+        </Link>
+    ));
 
     return (
         <section className="pv1 w-100 h2 sans-serif">
             {control}
 
-            <FontAwesome
-                className={"fr f5 link dim b no-underline black-50 dib pl2 pl3-ns pointer " + (layout === 'inline' ? 'black' : '')}
-                name={"align-justify"}
-                onClick={() => handleLayoutChange('inline')}/>
-            <FontAwesome
-                className={"fr f5 link dim b no-underline black-50 dib pl2 pl3-ns pointer " + (layout === 'grid' ? 'black' : '')}
-                name={"th-large"}
-                onClick={() => handleLayoutChange('grid')}/>
+            <div className="dn di-ns">
+                <FontAwesome
+                    className={
+                        "fr f5 link b no-underline black-50 dib pl2 pl3-ns pointer " +
+                        (layout === "inline" ? "black" : "")
+                    }
+                    name={"align-justify"}
+                    onClick={() => handleLayoutChange("inline")}
+                />
+                <FontAwesome
+                    className={
+                        "fr f5 link b no-underline black-50 dib pl2 pl3-ns pointer " +
+                        (layout === "grid" ? "black" : "")
+                    }
+                    name={"th-large"}
+                    onClick={() => handleLayoutChange("grid")}
+                />
+            </div>
         </section>
-    )
+    );
 };
 
 Controls.propTypes = {
@@ -34,7 +60,7 @@ Controls.propTypes = {
     layout: PropTypes.string.isRequired,
     handleLayoutChange: PropTypes.func.isRequired,
     handleTypeChange: PropTypes.func.isRequired,
-    query: PropTypes.string,
+    query: PropTypes.string
 };
 
-export {Controls};
+export { Controls };
