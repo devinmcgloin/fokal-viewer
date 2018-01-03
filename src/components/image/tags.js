d8ddc48875745f5d995e550d4063484c02ec9308
Devin McGloin <devin@devinmcgloin.com>
2018-01-02 23:38:03 -0800

    Increased width for tags

--- src/components/image/tags.js
+++ src/components/image/tags.js
@@ -14,7 +14,7 @@ const Tags = ({ tags }) => {
     ));
 
     return (
-        <div className="sans-serif center w-80 flex justify-center flex-wrap">
+        <div className="sans-serif center w-90 flex justify-center flex-wrap">
             {t}
         </div>
     );
