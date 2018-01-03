604e8470ddd3a66038410a312b6bfbdebc18f7ad
Devin McGloin <devin@devinmcgloin.com>
2018-01-02 23:07:11 -0800

    Improved image location tag linking

--- src/components/image/location.js
+++ src/components/image/location.js
@@ -3,11 +3,8 @@ import FontAwesome from "react-fontawesome";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-const Location = ({ description, point }) => (
-    <Link
-        to={"/search/images?q=" + encodeURIComponent(description)}
-        className="link pointer dim br2 ba ph3 pv2 mb2 dib black"
-    >
+const Location = ({ description, point }) => {
+    const content = (
         <span className="flex justify-between">
             <FontAwesome className="flex" name="map-marker" />
             <span className="sans-serif pl3 f6">
@@ -15,8 +12,19 @@ const Location = ({ description, point }) => (
                     point.lat.toFixed(3) + ", " + point.lng.toFixed(3)}
             </span>
         </span>
-    </Link>
-);
+    );
+
+    return description ? (
+        <Link
+            to={"/search/images?q=" + encodeURIComponent(description)}
+            className="link pointer dim br2 ba ph3 pv2 mb2 dib black"
+        >
+            {content}
+        </Link>
+    ) : (
+        <span className="br2 ba ph3 pv2 mb2 dib black">{content}</span>
+    );
+};
 
 Location.propTypes = {
     description: PropTypes.string,
