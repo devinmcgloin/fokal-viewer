5a2ee5c5eccb86aa333448297874082606fe4df9
Devin McGloin <devin@devinmcgloin.com>
2018-01-02 22:48:09 -0800

    Improved image uploading

--- src/scenes/manage/upload.js
+++ src/scenes/manage/upload.js
@@ -8,6 +8,7 @@ import { ErrorAlert, InfoAlert, SuccessAlert } from "../../components/alerts";
 import { Loading } from "../../components/loading";
 import { Exif, Tags, Geo, Gear } from "./images/patch";
 import { LoggedIn } from "../../services/store/auth";
+import { Image } from "../../components/image";
 import Dropzone from "react-dropzone";
 import FontAwesome from "react-fontawesome";
 
@@ -73,21 +74,21 @@ class ImageModify extends Component {
             );
         else
             stage = (
-                <div className="center tc pa3">
+                <div className="pa3">
                     <p className="lh-copy">
                         You can upload more photos you are proud of or view
                         other peoples images.
                     </p>
-                    <div className="center flex justify-around">
+                    <div className="fl">
                         <Link
                             to="/upload"
-                            className="f6 br2 ph5 pv3 mb2 dib shadow-5 bn glow pointer inline-flex items-center bg-animate bg-blue hover-bg-dark-blue white no-underline"
+                            className="f6 br2 ph5-l ph4 pv3 mb2 mr3 dib shadow-5 bn glow pointer inline-flex items-center bg-animate bg-blue hover-bg-dark-blue white no-underline"
                         >
                             Upload
                         </Link>
                         <Link
                             to="/"
-                            className="f6 br2 ph5 pv3 mb2 dib shadow-5 bn glow pointer inline-flex items-center bg-animate bg-blue hover-bg-dark-blue white no-underline"
+                            className="f6 br2 ph5-l ph4 pv3 mb2 dib shadow-5 bn glow pointer inline-flex items-center bg-animate bg-blue hover-bg-dark-blue white no-underline"
                         >
                             Browse
                         </Link>
@@ -95,9 +96,16 @@ class ImageModify extends Component {
                 </div>
             );
         return (
-            <div className="sans-serif pa3 center">
-                <img className="mb3" src={this.state.image.src_links.medium} />
-                {stage}
+            <div className="sans-serif pa3 center w-90">
+                <div className="w-100 w-50-m w-50-l db dtc-ns v-mid-ns">
+                    <Image
+                        className="w-100"
+                        url={this.state.image.src_links.raw}
+                    />
+                </div>
+                <div className="db dtc-ns v-mid mw6 w-100 w-50-ns mt2 mt0-ns">
+                    <span className="db">{stage}</span>
+                </div>
             </div>
         );
     }
